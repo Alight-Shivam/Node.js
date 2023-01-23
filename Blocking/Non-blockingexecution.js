a58c11ee5a54f584059675b2f61be8cb60c01352
Alight-Shivam <97473543+Alight-Shivam@users.noreply.github.com>
2023-01-23 09:56:13 +0530

    node.js

---
+++ Blocking/Non-blockingexecution.js
@@ -0,0 +1,9 @@
+  // Synchronous or Blocking (line by line execution)
+
+  // Asynchronous or Non-Blocking (line by line execution not guaranteed, callbacks will fire)
+
+  const fs = require("fs");
+  fs.readFile("delete.txt", "utf-8", (err, data)=>{
+    console.log(data);
+  });
+  console.log("This is a message");
